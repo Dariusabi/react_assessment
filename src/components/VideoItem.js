98c3ee9cfb77e02d255b102fa296a56088cb7efe
Dragos Rusea <rusea@unfuddle.com>
2020-06-16 20:51:41 +0300

    Make the youtube embeded video auto play.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -12,7 +12,7 @@ const VideoItem = ({video, handleVideoSelect}) => {
                 </div>
             </div>
             
-            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.id.videoId}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.id.videoId}?autoplay=1`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import SearchPage from "./components/SearchPage";
 
 function App() {
-
     return (<SearchPage/>);
 }
 
